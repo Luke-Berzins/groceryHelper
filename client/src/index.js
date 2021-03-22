00957c6515ee13b27c2c89ceca038cb16e2c5a49
Luke Berzins <lukeberzins16@gmail.com>
2021-03-22 18:02:41 -0400

    successful get request to graphql api

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { gql } from '@apollo/client';
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache()
+})
+
+client
+  .query({
+    query : gql`
+       query {
+         books {
+        title
+        author
+      }
+    }
+      `
+  }).then(res => console.log(res))
 
 ReactDOM.render(
   <React.StrictMode>
