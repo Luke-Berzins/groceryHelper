79abe3324ebedaf9fb9ba104ee7be6564b1ec130
Luke Berzins <lukeberzins16@gmail.com>
2021-03-23 17:49:36 -0400

    intial configuration complete, starting development branch

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,36 +1,35 @@
 import { act, render, screen } from '@testing-library/react';
-import TestRenderer from 'react-test-renderer';
 import { MockedProvider } from '@apollo/client/testing';
 // The component AND the query need to be exported
 import { bookList, InitialQuery } from './InitialQuery';
 import { waitForElementToBeRemoved } from "@testing-library/react";
 
 
+const mocks = [
+  {
+    request: {
+      query: bookList
+    },
+    result: {
+      data: {
+        books:  [
+          {
+            "title": "The Awakening",
+            "author": "Kate Chopin"
+          },
+          {
+            "title": "City of Glass",
+            "author": "Paul Auster"
+          }
+        ]
+      
+      },
+    },
+  },
+];
 
 describe("InitialTests", () => {
-  it('should render dog', async () => {
-    const mocks = [
-      {
-        request: {
-          query: bookList
-        },
-        result: {
-          data: {
-            books:  [
-              {
-                "title": "The Awakening",
-                "author": "Kate Chopin"
-              },
-              {
-                "title": "City of Glass",
-                "author": "Paul Auster"
-              }
-            ]
-          
-          },
-        },
-      },
-    ];
+  it('should render responses from the database', async () => {
     
     const { getByText } = render(
       <MockedProvider mocks={mocks} addTypename={false}>
