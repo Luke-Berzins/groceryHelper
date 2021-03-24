60f3433211fb995045ce51c7044e04ad400a0b91
Luke Berzins <lukeberzins16@gmail.com>
2021-03-24 15:53:33 -0400

    schema setup for app data, ingredients users, and recipes

--- graphql-server-groceryHelper/index.js
+++ graphql-server-groceryHelper/index.js
@@ -1,14 +1,10 @@
 const { ApolloServer } = require('apollo-server');
 const  typeDefs = require('./schema')
 const resolvers = require('./resolvers')
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// The `listen` method launches a web server.
+
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
--- graphql-server-groceryHelper/schema.js
+++ graphql-server-groceryHelper/schema.js
@@ -1,14 +1,28 @@
 const { gql } = require('apollo-server');
 
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+
+
 const typeDefs = gql`
 # This "Book" type defines the queryable fields for every book in our data source.
-type Book {
-  title: String
-  author: String
+Users {
+  id: ID!
+  name: String!
+  username: String!
+  password: String!
 }
 type Ingredient {
   id: ID!
-  title: String!
+  name: String!
+}
+
+type Recipe {
+  id: ID!
+  name: String!
+  
 }
 
 # The "Query" type is special: it lists all of the available queries that
