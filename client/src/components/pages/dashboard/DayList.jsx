73afc6ffe4bfbe4939337c03e361022b495ae2c9
Luke Berzins <lukeberzins16@gmail.com>
2021-03-30 16:07:48 -0400

    adding schedule that shows 14 months into the future

--- client/src/components/pages/dashboard/DayList.jsx
+++ client/src/components/pages/dashboard/DayList.jsx
@@ -1,12 +1,16 @@
 export default function DayList() {
-  let dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Saturday']
-  let today = new Date()
+  let dayList = []
+  let today = new Date() 
+  for (let i = 0; i < 13; i++) {
+    const month= ["January","February","March","April","May","June","July",
+    "August","September","October","November","December"];
+    let calandar = new Date(today.setDate(today.getDate() + 1 ))
+    dayList.push(month[calandar.getMonth()] + ' ' + calandar.getDate()) 
+  }
 
 
 
   return dayList.map(day => {
-    return <div>{today.toString()}</div> 
-    
-    
+    return <div>{day}</div> 
   })
 }
