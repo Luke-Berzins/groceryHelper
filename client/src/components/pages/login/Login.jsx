5921d12afd0479da8e3ba8098072ed79fb9248d5
Luke Berzins <lukeberzins16@gmail.com>
2021-03-31 17:18:48 -0400

    adding login component

---
+++ client/src/components/pages/login/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Login(props) {
+
+
+
+  return (
+    <div>
+      <form>
+       <input placeholder="whatup"></input> 
+       <button>Hi</button>
+      </form>
+
+
+    </div>
+  )
+}
