82279e3cef31a7cba91ebc0624cb4f53a42a1c06
Luke Berzins <lukeberzins16@gmail.com>
2021-03-31 18:37:37 -0400

    login form hook prevents default submit and takes in user data

--- client/src/components/pages/login/Login.jsx
+++ client/src/components/pages/login/Login.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
+import { ValuesOfCorrectTypeRule } from 'graphql';
+import React, { useState } from 'react'
+import { useCookies } from 'react-cookie'
+import  useForm  from '../../../hooks/useForm'
+import './Login.scss'
+
 
 export default function Login(props) {
+  const { handleChange, handleSubmit, values } = useForm()
+  // const [cookies, setCookie] = useCookies(['name'])
+  
 
 
 
   return (
     <div>
-      <form>
-       <input placeholder="whatup"></input> 
-       <button>Hi</button>
+      <div className="zform">
+      
+      <form onSubmit={handleSubmit}>
+        <input type="text" 
+               name="name"
+               placeHolder="name"
+               value={values.name}
+               onChange={handleChange}
+        ></input> 
+        <button type="submit" >Hi</button>
       </form>
-
-
+      </div>
     </div>
   )
 }
